f90a3eccad181b95919f0c834b8eca820bc82c97
zhzkdls <gkdud3558@naver.com>
2022-03-26 16:32:57 +0900

    update

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -20,7 +20,7 @@ function NavBar() {
       <Navbar bg="light" expand={false}>
         <Container fluid>
           <Link to={"/"}>
-            <img src="./images/로고.jpg" alt="Xbox Logo" id="logo" />
+            <img src="./img/로고.jpg" alt="Xbox Logo" id="logo" />
             <span id="logoText">GO HOME</span>
           </Link>
           <Nav.Link href="#deets"></Nav.Link>
@@ -47,7 +47,7 @@ function NavBar() {
               <Nav className="justify-content-end flex-grow-1 pe-3">
                 <Nav.Link href="/">Home</Nav.Link>
                 <Nav.Link href="/login">이용안내</Nav.Link>
-                <Nav.Link href="/login">공지사항</Nav.Link>
+                <Nav.Link href="/board">공지사항</Nav.Link>
                 <Nav.Link href="/login">민원게시판</Nav.Link>
                 <Nav.Link href="/login">마이페이지</Nav.Link>
                 <Nav.Link href="/Reservation">예약내역</Nav.Link>
