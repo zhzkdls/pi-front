e35b6154ed5e936c0ee8f4261bb5d95289ff2185
YeongSeon1 <tjdudtjs1101@gmail.com>
2022-04-25 10:51:15 +0900

    asd

--- src/components/oauth/OAuth.js
+++
@@ -1,4 +0,0 @@
-const CLIENT_ID = "	d8569a424229ba2af3c521225133935e";
-const REDIRECT_URI = "http://localhost:3000/oauth/callback/kakao";
-
-export const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=code`;
