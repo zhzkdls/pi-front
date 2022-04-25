990cc279e7e078e5e82ca8cb67b9f9245d9087f7
YeongSeon1 <tjdudtjs1101@gmail.com>
2022-04-25 16:07:51 +0900

    asd

--- src/_actions/types.js
+++ src/_actions/types.js
@@ -9,7 +9,7 @@ export const LOGOUT_USER = "LOGOUT_USER";
 // export const BOARDBACKEND = "http://192.168.0.36:8083";
 
 export const RESERVATIONBACKEND = "http://54.153.15.108:8080";
-export const FACITDETAIL = "http://54.153.15.108";
+export const FACITDETAIL = "http://52.8.3.56";
 export const MEMBERBACKEND = "http://18.144.161.2:8080";
 export const BOARDBACKEND = "http://50.18.112.238:8080";
 
