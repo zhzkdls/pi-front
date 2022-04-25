738feadbfc66573fcc72d3dd748ba346070de3c2
YeongSeon1 <tjdudtjs1101@gmail.com>
2022-04-25 14:41:14 +0900

    asd

--- src/_actions/types.js
+++ src/_actions/types.js
@@ -8,9 +8,9 @@ export const LOGOUT_USER = "LOGOUT_USER";
 // export const MEMBERBACKEND = "http://192.168.0.36";
 // export const BOARDBACKEND = "http://192.168.0.36";
 
-export const RESERVATIONBACKEND = "http://10.0.133.216:8080";
-export const MEMBERBACKEND = "http://10.0.133.115:8080";
-export const BOARDBACKEND = "http://10.0.138.105:8080";
+export const RESERVATIONBACKEND = "http://54.153.15.108:8080";
+export const MEMBERBACKEND = "http://18.144.161.2:8080";
+export const BOARDBACKEND = "http://50.18.112.238:8080";
 
 export const WEATHERAPI = {
     key: "a7c2f342c4be7902411e89d9b854427d",
