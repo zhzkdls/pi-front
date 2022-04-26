5cb104d4d61b0c106925e28d4a2a0f04c6156f63
YeongSeon1 <tjdudtjs1101@gmail.com>
2022-04-26 10:57:46 +0900

    asd

--- src/_actions/types.js
+++ src/_actions/types.js
@@ -5,15 +5,15 @@ export const LOGOUT_USER = "LOGOUT_USER";
 
 
 // export const RESERVATIONBACKEND = "http://192.168.0.36:8081";
-export const RESERVATIONBACKEND = "http://192.168.28.34:8081";
-export const MEMBERBACKEND = "http://192.168.0.36:8080";
-export const BOARDBACKEND = "http://192.168.0.36:8083";
-export const FACITDETAIL = "http://localhost";
+// export const RESERVATIONBACKEND = "http://192.168.28.34:8081";
+// export const MEMBERBACKEND = "http://192.168.0.36:8080";
+// export const BOARDBACKEND = "http://192.168.0.36:8083";
+// export const FACITDETAIL = "http://localhost";
 
-// export const RESERVATIONBACKEND = "http://54.153.15.108:8080";
-// export const FACITDETAIL = "http://52.8.3.56";
-// export const MEMBERBACKEND = "http://18.144.161.2:8080";
-// export const BOARDBACKEND = "http://50.18.112.238:8080";
+export const RESERVATIONBACKEND = "http://54.153.15.108:8080";
+export const FACITDETAIL = "http://52.8.3.56";
+export const MEMBERBACKEND = "http://18.144.161.2:8080";
+export const BOARDBACKEND = "http://50.18.112.238:8080";
 
 
 export const WEATHERAPI = {
